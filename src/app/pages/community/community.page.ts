c73f1ad38072843ef0b84d6ceb1a61edfd6b6d95
Alessandro Petrini <petrinialessandro14@gmail.com>
2022-09-11 12:40:47 +0200

    community page add and delete friend

--- src/app/pages/community/community.page.ts
+++ src/app/pages/community/community.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { AlertController } from '@ionic/angular';
+import { LoadingService } from '../../services/loading.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-community',
@@ -8,20 +11,54 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class CommunityPage implements OnInit {
   profile=null;
-  friends:string[];
-  constructor(private userService: UserService) { 
+  friends;
+  friendInfo:FormGroup;
+
+  constructor(private userService: UserService, private loadingService:LoadingService, private alertController:AlertController, private fb:FormBuilder) { 
+    this.friendInfo=this.fb.group({username: ['', [Validators.required, Validators.minLength(3)]]});    
     this.userService.getUserProfile().subscribe((data)=>{
       this.profile=data;
-      this.friends=this.profile.FriendList;
     });
+  }
+
+  get username(){
+    return this.friendInfo.get('username');
   }
 
   ngOnInit() {
     this.loadFriends();
   }
 
-  loadFriends(){
-    this.userService.getFriendList();
+  async loadFriends(){    
+    this.friends=await this.userService.getFriendList();
   }
+
+  async removeFriend(friend){
+    await this.loadingService.present({ message: 'Deleting friend',duration: 5000 }); 
+    await this.userService.deleteFriend(friend);
+    await this.loadingService.dismiss();
+    await this.loadFriends();
+  }
+
+  async addFriend(){
+    await this.loadingService.present({ message: 'Adding friend',duration: 5000 });       
+    await this.loadingService.dismiss();
+    if(!await this.userService.addFriend(this.friendInfo.get('username').value)){
+      this.showAlert("Error","User not found");
+    }
+    await this.loadFriends();
+
+    await this.loadFriends();
+  }
+
+  chat(friend){
+    this.showAlert("Attention","Chat service is not available at the moment.");
+  }
+
+  async showAlert(header, message){
+    const alert = await this.alertController.create({header,message,buttons: ['OK']});
+    await alert.present();
+  }
+  
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,7 +3,7 @@ import { Auth } from '@angular/fire/auth';
 import { Firestore, doc, docData, setDoc, collection, collectionData } from '@angular/fire/firestore';
 import { Storage, ref } from '@angular/fire/storage';
 import { Photo } from '@capacitor/camera';
-import { getDocs, query, where } from 'firebase/firestore';
+import { arrayRemove, arrayUnion, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import { getDownloadURL, uploadString } from 'firebase/storage';
 import { Observable } from 'rxjs';
 
@@ -13,6 +13,7 @@ export interface User{
   Death: number;
   TravelDist: number;
   Xp: number;
+  FriendList: string[];
   imageUrl: string;
   Nickname: string;
 }
@@ -37,8 +38,8 @@ export class UserService {
     const defaultImageUrl="https://firebasestorage.googleapis.com/v0/b/pbcompanion.appspot.com/o/uploads%2FaM3ECTh0wDZlu8FHA5LjZoZUAak1%2Fprofile.png?alt=media&token=9ec5d0d2-6902-4095-9869-fcefb0a9c95e";
     try{      
       const userDocReference = doc(this.firestore,`users/${user.uid}`);
-      let FriendList= new Array<string>();
-       setDoc(userDocReference,{imageUrl:defaultImageUrl, Death:"0", Kills:"0", FriendList, TravelDist: "0", Xp: "0", Nickname:username});
+      let FriendList:string[]=null;
+      setDoc(userDocReference,{imageUrl:defaultImageUrl, Death:"0", Kills:"0", FriendList:FriendList, TravelDist: "0", Xp: "0", Nickname:username});
       return true;
     } catch(e){
         return null;
@@ -53,7 +54,7 @@ export class UserService {
       await uploadString(storageReference, cameraFIle.base64String,'base64');
       const imageUrl = await getDownloadURL(storageReference);
       const userDocReference = doc(this.firestore,`users/${user.uid}`);
-      await setDoc(userDocReference,{imageUrl,});
+      await updateDoc(userDocReference,{imageUrl:imageUrl});
       return true;
     } catch(e){
         return null;
@@ -70,14 +71,73 @@ export class UserService {
     const UsersReference = collection(this.firestore,'users');    
     const q= query(UsersReference, where ("FriendList","array-contains", this.auth.currentUser.uid));
     const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-});
-      
+    var friends = new Array<User>();
+    var user:User;
+    querySnapshot.forEach((doc) => { 
+      user= doc.data() as User;
+      user.id= doc.id;
+      friends.push(user);        
+    });
+    return friends;
   }
 
-  
+  getUsersIds(users){
+    var usersIds=new Array<string>();
+    for(var i=0;i<users.length;i++){
+      var user=users[i];
+      usersIds.push(user.uid);
+    }
+    return usersIds;
+  }
+
+  deleteFriend(friend){    
+    let newFriendList=new Array<string>();
+    let newUserList=new Array<string>();
+    this.getUserProfile().subscribe(res=>{
+    const userDocReference = doc(this.firestore,`users/${res.id}`);
+    const friendDocReference = doc(this.firestore,`users/${friend.id}`);
+    updateDoc(userDocReference,{FriendList: arrayRemove(friend.id)});
+    updateDoc(friendDocReference,{FriendList:arrayRemove(res.id)});
+    });
+    return true;
+  } 
+
+  async addFriend(friendName){
+    let friendUser:User;
+    friendUser=await this.getUser(friendName);
+    if(friendUser!==null){
+      this.getUserProfile().subscribe((res)=>{ 
+      if(res.FriendList!==null){
+        for (let index = 0; index < res.FriendList.length; index++) {
+        let tempId = res.FriendList[index];
+        if(tempId===friendUser.id){
+          return false;
+        }        
+      } 
+    }
+                    
+      const userDocReference = doc(this.firestore,`users/${res.id}`);
+      const friendDocReference = doc(this.firestore,`users/${friendUser.id}`);
+      updateDoc(userDocReference,{FriendList: arrayUnion(friendUser.id) });
+      updateDoc(friendDocReference,{FriendList: arrayUnion(res.id)});      
+      });
+      return true;
+    } else{
+      return false; 
+    }  
+    
+  }
+
+  async getUser(nickname){
+    const UsersReference = collection(this.firestore,'users');    
+    const q= query(UsersReference, where ("Nickname","==", nickname));
+    const querySnapshot = await getDocs(q);
+    var retUser:User;
+    if(querySnapshot.size==0){
+      return null;
+    }
+    retUser=querySnapshot.docs[0].data() as User;
+    retUser.id=querySnapshot.docs[0].id;
+    return retUser;
+  }
 }
-
-
